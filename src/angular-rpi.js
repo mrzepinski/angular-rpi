0eb70d3c45884d6bf74675c2d1038dae0a189d9f
Piotr Stępniewski <k3nsei.pl@gmail.com>
2016-05-05 12:49:40 +0200

    Refactoring and update
    This should be nicer. And will be work with current version RC.1

--- src/angular-rpi.js
+++ src/angular-rpi.js
@@ -1,38 +1,35 @@
-import {Component, View} from 'angular2/angular2';
+import {Component, HostListener} from '@angular/core';
 
 @Component({
-    selector: 'angular-rpi'
-})
-
-@View({
+    selector: 'angular-rpi',
     templateUrl: 'angular-rpi.html'
 })
 
 export class AngularRpi {
-
     constructor() {
         this.progress = '0%';
+        this.endPoint = 0;
 
-        let body = document.getElementsByTagName('body')[0],
-            endPoint = 0,
-            getEndPoint = function () {
-                return body.scrollHeight - window.innerHeight;
-            },
-            setProgress = function () {
-                let y = window.scrollY || window.pageYOffset;
-                this.progress = [(y / endPoint * 100), '%'].join('');
-            }.bind(this),
-            updateMetrics = function () {
-                endPoint = getEndPoint();
-                setProgress();
-            };
-
-        endPoint = getEndPoint();
-        updateMetrics();
-        window.addEventListener('scroll', setProgress);
-        window.addEventListener('resize', updateMetrics);
+        this.endPoint = this.getEndPoint();
+        this.updateMetrics();
 
         console.info('AngularRpi Component Mounted Successfully');
     }
 
+    getEndPoint() {
+        const body = document.getElementsByTagName('body')[0];
+        return body.scrollHeight - window.innerHeight;
+    }
+
+    @HostListener('window:scroll')
+    setProgress() {
+        const y = window.scrollY || window.pageYOffset;
+        this.progress = `${(y / this.endPoint * 100)}%`;
+    }
+
+    @HostListener('window:resize')
+    updateMetrics () {
+        this.endPoint = this.getEndPoint();
+        this.setProgress();
+    };
 }
